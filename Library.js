5314aa36e76ec0afc39331ee3a544fc512fba2b1
jdmartinez1062 <jd.martinez1062@gmail.com>
2020-11-10 15:59:41 -0500

    basic html skeleton

--- Library.js
+++ Library.js
@@ -1,8 +1,35 @@
 let myLibrary = [];
 
-function Book() {
-    // the constructor...
+function Book(title, author, number_pages, read) {
+    this.title = title;
+    this.author = author;
+    this.number_pages = number_pages;
+    this.read = read;
 }
+// this.check_read = function (read) {
+//     if (read == true) {
+//         return 'already read'
+//     } else {
+//         return 'not read yet'
+//     }
+// }
+
+// this.info = function () {
+//     return `${this.title} by ${this.author}, ${this.number_pages} pages, ${this.check_read(this.read)}.`
+// }
+Book.prototype.check_read = function (read) {
+    if (read == true) {
+        return 'already read'
+    } else {
+        return 'not read yet'
+    }
+}
+Book.prototype.info = function () {
+    return `${this.title} by ${this.author}, ${this.number_pages} pages, ${this.check_read(this.read)}.`
+}
+
+const a = new Book("Harry Potter", "IDK", 300, true);
+console.log(a.info());
 
 function addBookToLibrary() {
     // do stuff here
