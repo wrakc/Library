41697b40711b75004f1392a7a3b203994a703d96
wrakc <carlosveiga1618@gmail.com>
2020-11-12 14:49:03 -0300

    add update status of read or not read button
    add id atribute to each newly created book

--- DOM.js
+++ DOM.js
@@ -1,45 +1,59 @@
 function displayBooks(array) {
     for (let book in array) {
-
         const container = document.createElement('div');
         container.id = "container-" + array[book].title.replace(/\s+/g, '');
         const title = document.createElement('p');
         const author = document.createElement('p');
         const pages = document.createElement('p');
         const del = document.createElement('button');
+        const changeReadStatus = document.createElement('button');
+        const readStatus = document.createElement('p');
         del.textContent = "Delete book";
+        del.classList = "delete";
+        changeReadStatus.classList = "read"
+        readStatus.id = "p-read-" + array[book].title.replace(/\s+/g, '');
+        changeReadStatus.id = "readButton-" + array[book].title.replace(/\s+/g, '');
         del.id = "button-" + array[book].title.replace(/\s+/g, '');
-        console.log('title');
-        console.log();
         title.textContent = array[book].title;
         author.textContent = array[book].author;
         pages.textContent = array[book].number_pages;
+        readStatus.textContent = array[book].check_read();
+        changeReadStatus.textContent = array[book].opposite_status();
         container.appendChild(title);
         container.appendChild(author);
         container.appendChild(pages);
+        container.appendChild(readStatus);
+        container.appendChild(changeReadStatus);
         container.appendChild(del);
         document.getElementById('main').appendChild(container);
     }
 }
 
 function update(book) {
-
     const container = document.createElement('div');
     container.id = "container-" + book.title.replace(/\s+/g, '');
     const title = document.createElement('p');
     const author = document.createElement('p');
     const pages = document.createElement('p');
     const del = document.createElement('button');
+    const readStatus = document.createElement('p');
+    const changeReadStatus = document.createElement('button');
     del.textContent = "Delete book";
     del.id = "button-" + book.title.replace(/\s+/g, '');
-    console.log('title');
-    console.log();
+    del.classList = "delete";
+    changeReadStatus.classList = "read"
+    readStatus.id = "p-read-" + book.title.replace(/\s+/g, '');
+    changeReadStatus.id = "readButton-" + book.title.replace(/\s+/g, '');
     title.textContent = book.title;
     author.textContent = book.author;
     pages.textContent = book.number_pages;
+    readStatus.textContent = book.check_read();
+    changeReadStatus.textContent = book.opposite_status();
     container.appendChild(title);
     container.appendChild(author);
     container.appendChild(pages);
+    container.appendChild(readStatus);
+    container.appendChild(changeReadStatus);
     container.appendChild(del);
     document.getElementById('main').appendChild(container);
     document.getElementById(del.id).addEventListener("click", function (e) {
@@ -54,4 +68,9 @@ function deleteBook(book) {
     console.log(book);
     console.log("executed");
     document.getElementById('container-' + book).remove();
+}
+
+function updateReadStatus(book) {
+    document.getElementById('p-read-' + book).textContent = book.check_read();
+    document.getElementById('readButton-' + book).textContent = book.opposite_status();
 }
--- Library.js
+++ Library.js
@@ -1,25 +1,40 @@
 let myLibrary = [];
 
-function Book(title, author, number_pages, read) {
+function Book(title, author, number_pages, read, id) {
     this.title = title;
     this.author = author;
     this.number_pages = number_pages;
     this.read = read;
+    this.id = id;
 }
 
-Book.prototype.check_read = function (read) {
-    if (read == true) {
+Book.prototype.check_read = function () {
+    if (this.read == true) {
         return 'already read'
     } else {
         return 'not read yet'
     }
 }
+
+Book.prototype.opposite_status = function () {
+    if (this.read == true) {
+        return 'Change to "Not read yet"'
+    } else {
+        return 'Change to "Read"'
+    }
+}
+
+
+Book.prototype.toggle_read = function () {
+    this.read ? false : true;
+}
+
 Book.prototype.info = function () {
     return `${this.title} by ${this.author}, ${this.number_pages} pages, ${this.check_read(this.read)}.`
 }
 
-const a = new Book("Harry Potter", "IDK", 300, true);
-const b = new Book("Rails", "IDK", 300, true);
+const a = new Book("Harry Potter", "IDK", 300, true, 0);
+const b = new Book("Rails", "IDK", 300, true, 1);
 console.log(a.info());
 
 function addBookToLibrary(book) {
@@ -31,7 +46,9 @@ function newBook() {
     let author = document.getElementById('author').value;
     let pages = document.getElementById('pages').value;
     let read = document.getElementById('read').value;
-    const bookNew = new Book(title, author, pages, read);
+    let id = checkFirstId(myLibrary);  
+    changeReadValue(read);
+    const bookNew = new Book(title, author, pages, read, id);
     addBookToLibrary(bookNew);
     form = document.getElementById("form");
     form.classList.toggle("hidden");
@@ -43,9 +60,26 @@ function showForm() {
     form.classList.toggle("hidden");
 }
 
+function changeReadValue(value) {
+    if (value == 'true') {
+        value = true;
+    } else {
+        value = false;
+    };
+}
+
+function checkFirstId(element) {
+    if (element.length == 0) {
+        return 0
+    } else {
+        return element[element.length - 1].id + 1;
+    }
+}
+
 function deleteArray(book) {
     myLibrary.indexOf(book.title);
 }
+
 document.getElementById("btn-library").addEventListener("click", newBook);
 
 document.getElementById("new-book").addEventListener("click", showForm);
@@ -53,12 +87,9 @@ document.getElementById("new-book").addEventListener("click", showForm);
 function deleteDiv(e) {
     e = e.target || e.srcElement;
     let b = e.id;
-    if (b != "new-book") {
-        console.log(b);
-        let k = b.substring(7, b.length);
-        deleteBook(k);
-        deleteArray(k);
-    }
+    let k = b.substring(7, b.length);
+    deleteBook(k);
+    deleteArray(k);
 };
 
 addBookToLibrary(a);
@@ -66,12 +97,19 @@ addBookToLibrary(b);
 console.log(myLibrary);
 displayBooks(myLibrary);
 
-objects = Array.from(document.getElementsByTagName("button"));
+objects = Array.from(document.getElementsByClassName("delete"));
+objectsRead = Array.from(document.getElementsByClassName("read"));
 
 for (var i = 0; i < objects.length; i++) {
     document.getElementById(objects[i].id).addEventListener("click", deleteDiv);
+    document.getElementById(objectsRead[i].id).addEventListener("click", function (e) {
+        e = e.target || e.srcElement;
+        myLibrary.indexOf('')
+        console.log(e);
+        e.toggle_read();
+        let b = e.id;
+        console.log(b);
+        let k = b.substring(10, b.length);
+        updateReadStatus(k)
+    });
 };
-
-
-
-
